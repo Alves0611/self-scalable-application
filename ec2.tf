c2b7f1f4b9d069014f829adcdc2acd3960a07960
Alves0611 <g.alves0611@gmail.com>
2024-02-17 13:28:04 -0300

    instance created

--- ec2.tf
+++ ec2.tf
@@ -22,3 +22,16 @@ resource "aws_launch_template" "this" {
   }
 }
 
+resource "aws_instance" "jenkins" {
+  ami           = var.instance_config.ami
+  instance_type = var.instance_config.type
+
+  vpc_security_group_ids = [aws_security_group.jenkins.id]
+  subnet_id              = aws_subnet.this["pvt_a"].id
+  availability_zone      = aws_subnet.this["pvt_a"].availability_zone
+
+  tags = {
+    "Name" = "${local.namespaced_service_name}-jenkins"
+  }
+}
+
